a9fe7841d87cae8b87cd64111d89799b3ddc3e68
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-04-04 21:49:47 +0530

    tokenizing and making sense of some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use std::io::{stdin, stdout, Write};
-
+use std::process::exit;
+use std::rc::Rc;
 use regex::Regex;
+
+enum MalType {
+    Nil,
+    Bool(bool),
+    Int(i32),
+    Sym(String),
+    List(Rc<Vec<MalType>>),
+}
+
+enum MalErr {
+    ParseErr(String),
+}
 
 struct Reader {
     tokens: Vec<String>,
@@ -10,7 +23,7 @@ struct Reader {
 impl Reader {
     fn next(&mut self) -> Option<String> {
         self.pos = self.pos + 1;
-        match self.tokens.get(self.pos) {
+        match self.tokens.get(self.pos - 1) {
             Some(token) => Some(token.to_owned()),
             None => None,
         }
@@ -40,7 +53,69 @@ fn tokenize(s: &str) -> Vec<String> {
     vec
 }
 
-fn read_str() {
+fn read_list(rd: &mut Reader, end: &str) -> Result<MalType, MalErr> {
+    let mut vec: Vec<MalType> = vec![];
+    loop {
+        let token = match rd.peek() {
+            Some(t) => t,
+            None => {
+                eprintln!("Expected {end} but found EOF");
+                exit(1);
+            }
+        };
+        if token == end {
+            break;
+        }
+        vec.push(read_form(rd)?);
+    }
+    let _ = rd.next(); // skip ")"
+    Ok(MalType::List(Rc::new(vec)))
+}
+
+fn read_atom(rd: &mut Reader) -> MalType {
+    let num_re = Regex::new(r"^-?[0-9]+$").expect("Invalid regular expression for number");
+    match rd.next() {
+        Some(token) => match &token[..] {
+            "nil" => MalType::Nil,
+            "true" => MalType::Bool(true),
+            "false" => MalType::Bool(false),
+            _ => {
+                if num_re.is_match(&token) {
+                    MalType::Int(token.parse().unwrap())
+                } else {
+                    MalType::Sym(token.to_string())
+                }
+            }
+            // any_thing => MalType::Int(any_thing.parse::<i32>().expect("unable to parse int")),
+        }
+        None => {
+            eprintln!("Expected token found none");
+            exit(1);
+        }
+    }
+}
+
+fn read_form(rd: &mut Reader) -> Result<MalType, MalErr> {
+    match rd.peek() {
+        Some(token) => match &token[..] {
+            "(" => {
+                let _ = rd.next();
+                read_list(rd, ")")
+            }
+            _ => Ok(read_atom(rd)),
+        }
+        None => Err(MalErr::ParseErr("No tokens found".to_string())),
+    }
+}
+
+fn read_str(s: &str) {
+    let tokens = tokenize(&s);
+    // println!("{:?}", tokens);
+    let mut reader = Reader {
+        tokens,
+        pos: 0,
+    };
+    let _ = read_form(&mut reader);
 }
 
 fn rpl() { // read print loop
@@ -49,7 +124,11 @@ fn rpl() { // read print loop
         print!("mal> ");
         stdout().flush().expect("Failed to flush prompt");
         stdin().read_line(&mut buf).expect("Failed to read stdin");
-        print!("{buf}");
+
+        if !buf.is_empty() {
+            read_str(&buf);
+        }
+
         buf.clear();
     }
 }
