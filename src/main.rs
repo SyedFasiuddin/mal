e4759f8faa79be4950715ce910fc1354e9d79445
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-04-04 15:59:38 +0530

    impl reader which stores n gives tokens on demand

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
 use std::io::{stdin, stdout, Write};
 
+struct Reader {
+    tokens: Vec<String>,
+    pos: usize,
+}
+
+impl Reader {
+    fn next(&mut self) -> Option<String> {
+        self.pos = self.pos + 1;
+        match self.tokens.get(self.pos) {
+            Some(token) => Some(token.to_owned()),
+            None => None,
+        }
+    }
+
+    fn peek(&self) -> Option<String> {
+        match self.tokens.get(self.pos) {
+            Some(token) => Some(token.to_owned()),
+            None => None,
+        }
+    }
+}
+
 fn rpl() { // read print loop
     let mut buf = String::new();
     loop {
