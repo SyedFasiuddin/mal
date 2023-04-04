d0856d0fcdbee08ab7a0c03cb5b340a6c71e3946
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-04-04 13:31:13 +0530

    complete step 0: read print loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{stdin, stdout, Write};
+
+fn rpl() { // read print loop
+    let mut buf = String::new();
+    loop {
+        print!("mal> ");
+        stdout().flush().expect("Failed to flush prompt");
+        stdin().read_line(&mut buf).expect("Failed to read stdin");
+        print!("{buf}");
+        buf.clear();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    rpl();
 }
